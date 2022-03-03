5f83ceb197add0a789a67bbe9d959c4f8cf30c92
Anesu <elementstudios.sa@gmail.com>
2022-03-03 20:38:26 +0200

    first commit

---
+++ app/src/main/java/com/example/uberrider/Common/Common.java
@@ -0,0 +1,8 @@
+package com.example.uberrider.Common;
+
+import com.example.uberrider.Model.RiderModel;
+
+public class Common {
+    public static final String RIDER_INFOR_REFERENCE = "Riders";
+    public static RiderModel currentRider;
+}
